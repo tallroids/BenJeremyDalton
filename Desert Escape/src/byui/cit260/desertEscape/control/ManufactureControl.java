d7f323e5a0fd2e46867f830642d7483b4684ced8
tallroids <tallroids@Bens-HP>
2016-02-12 10:59:28 -0700

    2/12/16 Control Layer Initiation

---
+++ Desert Escape/src/byui/cit260/desertEscape/control/ManufactureControl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.desertEscape.control;
+
+/**
+ *
+ * @author tallroids
+ */
+public class ManufactureControl {
+    public double calcShovelVolume (double shovelLength, double shovelWidth, double shovelDepth){
+        if (shovelLength < 0 || shovelLength > 10) {
+            return -1;
+        } 
+        if (shovelWidth < 0 || shovelWidth > 10) {
+            return -1;
+        } 
+        if (shovelDepth < 0 || shovelDepth > 10) {
+            return -1;
+        }
+
+        double shovelVolume = shovelLength * shovelWidth * shovelDepth;
+
+        return shovelVolume;
+       }
+}
