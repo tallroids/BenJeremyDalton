1dfbb6db86f9a154fda559cd430eb23372b53117
tallroids <tallroids@Bens-HP>
2016-03-16 18:58:10 -0600

    3/12/16 Completed and implemented the calculatePercentComplete function called in the characterStatsView

---
+++ Desert Escape/src/byui/cit260/desertEscape/model/Scene.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.desertEscape.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author tallroids
+ */
+public class Scene {
+    private String descrption;
+    private Boolean blocked;
+
+    public Scene() {
+    }
+
+    public String getDescrption() {
+        return descrption;
+    }
+
+    public void setDescrption(String descrption) {
+        this.descrption = descrption;
+    }
+
+    public Boolean getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(Boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.descrption);
+        hash = 79 * hash + Objects.hashCode(this.blocked);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.descrption, other.descrption)) {
+            return false;
+        }
+        if (!Objects.equals(this.blocked, other.blocked)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Scene{" + "descrption=" + descrption + ", blocked=" + blocked + '}';
+    }
+    
+    
+}
