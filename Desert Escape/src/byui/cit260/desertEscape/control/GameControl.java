b5f744f7c7b424d9d3a6637e1dcd6612d6940d97
tallroids <tallroids@Bens-HP>
2016-03-16 18:56:35 -0600

    3/12/16 Completed and implemented the calculatePercentComplete function called in the characterStatsView

--- Desert Escape/src/byui/cit260/desertEscape/control/GameControl.java
+++ Desert Escape/src/byui/cit260/desertEscape/control/GameControl.java
@@ -5,9 +5,20 @@
  */
 package byui.cit260.desertEscape.control;
 
+import byui.cit260.desertEscape.control.MapControl;
 import byui.cit260.desertEscape.model.Game;
+import byui.cit260.desertEscape.model.Item;
+import byui.cit260.desertEscape.model.InventoryItem;
+import byui.cit260.desertEscape.model.Location;
+import byui.cit260.desertEscape.model.Map;
 import byui.cit260.desertEscape.model.Player;
+import byui.cit260.desertEscape.model.Scene;
+import byui.cit260.desertEscape.model.SceneType;
+import byui.cit260.desertEscape.model.TimeMachine;
 import desert.escape.DesertEscape;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
@@ -29,9 +40,22 @@ public class GameControl {
     }
 
     public static void createNewGame(Player player) {
-        Game game = new Game();
+        Game game = new Game();        
+
         game.setPlayer(player);
+        game.getPlayer().setPercentComplete(0.0);
+        
+        Item[] inventoryList = GameControl.createInventoryList();
+        game.setInventory(inventoryList);
+        
+        TimeMachine timeMachine = new TimeMachine();
+        game.setTimeMachine(timeMachine);
+        
+        Map map = MapControl.createMap();
+        game.setMap(map);        
+        
         DesertEscape.setCurrentGame(game);
+
     }
 
     public static void resumeGame(Player player) {
@@ -69,6 +93,63 @@ public class GameControl {
 
     public static void checkInventory(Player player) {
         System.out.println("checkInventory function called");
+    }
+
+    private static Item[] createInventoryList() {
+        Item[] inventory = new Item[5];
+        
+        Item shovel = new Item();
+        shovel.setDescription("shovel");
+        shovel.setItemPrice(0);
+        inventory[InventoryItem.shovel.ordinal()] = shovel;
+        
+        Item timeMachineParts = new Item();
+        timeMachineParts.setDescription("Parts to build time machine");
+        timeMachineParts.setItemPrice(0);
+        inventory[InventoryItem.timeMachineParts.ordinal()] = timeMachineParts;
+        
+        Item rayGun = new Item();
+        rayGun.setDescription("Shoot aliens with this bad-boy");
+        rayGun.setItemPrice(20);
+        inventory[InventoryItem.rayGun.ordinal()] = rayGun;
+        
+        return inventory;
+    }
+
+    static void assignScenesToLocations(Map map, Scene[] scenes) {
+        Location[][] locations = map.getLocations();
+        
+        locations[0][0].setScene(scenes[SceneType.prologueScene.ordinal()]);
+        locations[0][1].setScene(scenes[SceneType.alienCamp.ordinal()]);
+        locations[0][2].setScene(scenes[SceneType.caves.ordinal()]);
+        locations[1][0].setScene(scenes[SceneType.cliff.ordinal()]);
+        locations[1][1].setScene(scenes[SceneType.crevass.ordinal()]);
+        locations[1][2].setScene(scenes[SceneType.desert.ordinal()]);
+        locations[2][0].setScene(scenes[SceneType.mountains.ordinal()]);
+        locations[2][1].setScene(scenes[SceneType.pit.ordinal()]);
+        locations[2][2].setScene(scenes[SceneType.shop.ordinal()]);
+
+    }
+
+    public static Item[] getSortedInventoryList() {
+        
+        return null;
+    }
+
+    public static double calculatePercentComplete() {
+        Map map = DesertEscape.getCurrentGame().getMap();
+        Location[][] locations = DesertEscape.getCurrentGame().getMap().getLocations();
+            double rtn = 0.0;
+        for (int row = 0; row < map.getNoOfRows(); row++) {
+            for (int col = 0; col < map.getNoOfColumns(); col++) {
+                if (locations[row][col].getScene().getBlocked() == false) {
+                    rtn += 11.11;
+                }else
+                rtn += 0;
+            }
+            
+        }
+        return rtn;
     }
 
     
