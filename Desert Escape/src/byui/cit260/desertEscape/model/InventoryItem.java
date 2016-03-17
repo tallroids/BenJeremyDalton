9b3f9bf3f27dac8a2efb5aa728266b1466335fea
tallroids <tallroids@Bens-HP>
2016-03-16 18:58:06 -0600

    3/12/16 Completed and implemented the calculatePercentComplete function called in the characterStatsView

---
+++ Desert Escape/src/byui/cit260/desertEscape/model/InventoryItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.desertEscape.model;
+
+/**
+ *
+ * @author tallroids
+ */
+public enum InventoryItem {
+    shovel,
+    timeMachineParts,
+    rayGun;
+}
